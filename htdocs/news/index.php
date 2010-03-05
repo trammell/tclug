ef7ffb8f7ce6593014826e7f5269ae6746516319
John Trammell <jotr@voh.(none)>
2010-03-05 06:28:09 -0600

    2009 version

--- htdocs/news/index.php
+++ htdocs/news/index.php
@@ -1,61 +1,52 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-<head>
-        <title>TCLUG - Twin Cities Linux Users Group</title>
-<meta http-equiv="author" content="TCLUG Webmaster">
-<meta http-equiv="Reply-to" content="webmaster@mn-linux.org">
-<meta http-equiv="Keywords" content="Twin Cities Linux Users Group, Linux, Minnesota,
-Advocacy, Linux Resources, Linux Links, Mailing List, Local Events">
-<meta http-equiv="Description" content="The Twin Cities Linux User's Group is a 
-local Linux advocacy group dedicated to expanding the use of Linux, both
-professionally and personally.  The group is comprised entirely of 
-volunteers.">
-<style>
-body, center, form, hr, p, td, th { font-family: Helvetica,Verdana,Sans-Serif;  
-                     color: #000000;}
-
-.header {       font-family: Helvetica,Verdana,sans-serif;      
-                        font-weight: bold; 
-                        color: #000080;}
-
-.copy  {        font-family: Helvetica,Verdana,sans-serif;  
-                        color: #999999;}
-</style>
-</head>
-
-<body bgcolor=white background="/img/mn-linux_bg.gif" marginwidth="0" marginheight="0" topmargin="0" 
-leftmargin="0">
+<?
+$title="TCLUG - Twin Cities Linux Users Group";
+include("/usr/httpd/virtual/www.mn-linux.org/html/header.inc");
+?>
 <table border="0" cellpadding="0" cellspacing="0" width="751">
 <tr>
-        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt=""></td>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt="Twin Cities Linux Users Group - Minneapolis, Minnesota"></td>
 </tr>
 </table>
-<table border="0" cellpadding="5" cellspacing="0" width="751">
+<table border="0" cellpadding="5" cellspacing="0" width="95%">
 <tr>
-        <td width="600">
-<table width=600 border="0" align="center" cellspacing="0" cellpadding="1">
+        <td width="95%">
+<table width=95% border="0" align="center" cellspacing="0" cellpadding="1">
 <tr>
   <td bgcolor="#000000" align="center">
 <table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
 <tr>
       <td align="left"><div class="header" align="center">
-<font size=+2>TCLUG News</font></div>
+<font size=+2>Latest News</font></div>
 <P>
-<? include("latestnews.inc") ?>
+<?
+
+include ("../../include/wwwmnlinuxorg.inc");
+tclug_db_connect () or exit;
+
+$sql = "SELECT *,UNIX_TIMESTAMP(date) from news order by date desc";
+$result = mysql_query($sql);
+$numrows = mysql_numrows($result);
+
+print "<UL>";
+while ($row=mysql_fetch_array($result)) {
+                $stringdate = date("l, F d, Y",$row[3]);
+                print "<LI><B>$stringdate</B>\n";
+                print "<blockquote>";
+		print "$row[news]\n";
+		print "</blockquote>";
+}
+print "</UL>";
+?>
 
 </td>
 </tr>
 </table></td>
 </tr>
 </table>
-<CENTER>
-<P class="copy"><font size=-1>
-Please direct all questions, comments, and insults to <a 
-href="mailto:webmaster@mn-linux.org">webmaster@mn-linux.org</a>,<BR>
-or use the <a href="/feedback.php">Feedback form</a>.<BR>
-Hosting generously provided by <a href="http://www.real-time.com">Real-Time 
-Enterprises</a>.
-</CENTER>
+
+<? include("../footer.inc") ?>
+
+
 </td>
         <td width="151" align="right" valign="top">
 <table border="0" cellpadding="0" cellspacing="0" width="140">
