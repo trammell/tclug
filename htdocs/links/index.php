b154232ad6d1da47dcf18d7893c9b9849862264f
John Trammell <jotr@voh.(none)>
2010-03-05 09:13:22 -0600

    checkin / old

---
+++ htdocs/links/index.php
@@ -0,0 +1,44 @@
+<?php
+$title="TCLUG - Twin Cities Linux Users Group";
+include("../header.inc");
+?>
+<table border="0" cellpadding="0" cellspacing="0" width="751">
+<tr>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt="Twin Cities Linux Users Group - Minneapolis, Minnesota"></td>
+</tr>
+</table>
+<table border="0" cellpadding="5" cellspacing="0" width="95%">
+<tr>
+        <td width="95%">
+<table width=95% border="0" align="center" cellspacing="0" cellpadding="1">
+<tr>
+  <td bgcolor="#000000" align="center">
+<table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
+<tr>
+      <td align="left"><div class="header" align="center">
+<font size=+2>Linux Links</font></div>
+<P>
+<?php include("links.inc") ?>
+
+
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+<?php include("../footer.inc") ?>
+</td>
+        <td width="151" align="right" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="140">
+<tr>
+        <td align="right"><br>
+<?php include("../links.inc") ?>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<br>
+</body>
+</html>
