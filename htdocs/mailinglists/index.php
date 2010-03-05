efbb9bce13ba10682a6c5e24bbce579a4d4d6789
John Trammell <jotr@voh.(none)>
2010-03-04 22:08:10 -0600

    keep

---
+++ htdocs/mailinglists/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+        <title>TCLUG - Twin Cities Linux Users Group</title>
+<meta http-equiv="author" content="TCLUG Webmaster">
+<meta http-equiv="Reply-to" content="webmaster@mn-linux.org">
+<meta http-equiv="Keywords" content="Twin Cities Linux Users Group, Linux, Minnesota,
+Advocacy, Linux Resources, Linux Links, Mailing List, Local Events">
+<meta http-equiv="Description" content="The Twin Cities Linux User's Group is a 
+local Linux advocacy group dedicated to expanding the use of Linux, both
+professionally and personally.  The group is comprised entirely of 
+volunteers.">
+<style>
+body, center, form, hr, p, td, th { font-family: Helvetica,Verdana,Sans-Serif;  
+                     color: #000000;}
+
+.header {       font-family: Helvetica,Verdana,sans-serif;      
+                        font-weight: bold; 
+                        color: #000080;}
+
+.copy  {        font-family: Helvetica,Verdana,sans-serif;  
+                        color: #999999;}
+</style>
+<link REL="SHORTCUT ICON" HREF="/images/tclug.ico" >
+</head>
+
+<body bgcolor=white background="/img/mn-linux_bg.gif" marginwidth="0" marginheight="0" topmargin="0" 
+leftmargin="0">
+<table border="0" cellpadding="0" cellspacing="0" width="751">
+<tr>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt="Twin Cities Linux Users Group - Minneapolis, Minnesota"></td>
+</tr>
+</table>
+<table border="0" cellpadding="5" cellspacing="0" width="95%">
+<tr>
+        <td width="95%">
+<table width=95% border="0" align="center" cellspacing="0" cellpadding="1">
+<tr>
+  <td bgcolor="#000000" align="center">
+<table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
+<tr>
+      <td align="left"><div class="header" align="center">
+<font size=+2>Mailing Lists</font></div>
+<BR>
+
+Join the mailing list for tons of execellent information and ideas. The latest news concerning the group is almost always to be found on the list first. Please don't
+ post any job-related messages to the general list. All job posts should go to the tclug-job mailing list.
+<P>
+Go to the following pages to subscribe/unsubscribe:<P>
+<A HREF="http://mailman.mn-linux.org/mailman/listinfo/tclug-list">TCLUG General list</A>
+<blockquote>
+General mailing list for questions, answers, or anything else about linux.
+This list also includes TCLUG Announcements.
+</blockquote>
+<P>
+<A HREF="http://mailman.mn-linux.org/mailman/listinfo/tclug-announce">TCLUG Announcements list</A>
+<blockquote>
+TCLUG announcements only, such as meetings and installfest announcements.
+</blockquote>
+<P>
+<A HREF="http://mailman.mn-linux.org/mailman/listinfo/tclug-devel">TCLUG Development list</A>
+<blockquote>
+Development list for programming and development questions.
+</blockquote>
+<P>
+<A HREF="http://mailman.mn-linux.org/mailman/listinfo/tclug-jobs">TCLUG Jobs list</A>
+<blockquote>
+Looking for a linux related job? Want to post a linux related job? Then this is the list for you.
+</blockquote>
+<HR>
+      <div class="header" align="center">
+<font size=+2>TCLUG Mailing Lists Archives</font></div>
+<BR>
+<A HREF="http://archives.mn-linux.org/tclug-list/">General Mailing List Archive</A>
+<P>
+<A HREF="http://archives.mn-linux.org/tclug-announce/">Announcements Mailing List Archive</A>
+<P>
+<A HREF="http://archives.mn-linux.org/tclug-jobs/">Jobs Mailing List Archive</A>
+<P>
+<HR>
+      <div class="header" align="center">
+<font size=+2>TCLUG Mailing List Etiquette</font></div>
+Before posting to the TCLUG mailing list, please read the following FAQ. It is
+a generic document not specific to the TCLUG, but it will explain the proper
+etiquette when posting to the list. Failure to comply will probably result in
+extreme flaming, especially from Chewie.<BR>
+<A HREF="http://www.gweep.ca/~edmonds/usenet/ml-etiquette.html">Mailing List
+Etiquette FAQ</A>
+
+<P><BR>
+
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+<CENTER>
+<P class="copy"><font size=-1>
+Please direct all questions, comments, and insults to <a 
+href="mailto:webmaster@mn-linux.org">webmaster@mn-linux.org</a>,<BR>
+or use the <a href="/feedback.php">Feedback form</a>.<BR>
+Hosting generously provided by <a href="http://www.real-time.com">Real-Time 
+Enterprises</a>.
+</CENTER>
+</td>
+        <td width="151" align="right" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="140">
+<tr>
+        <td align="right"><br>
+<? include("../links.inc") ?>
+</td>
+</tr>
+</table>        
+</td>
+</tr>
+</table>
+<br>
+</body>
+</html>
