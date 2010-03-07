064a35b0321bd70b13cf98dfddea3888e77b883a
John Trammell <jotr@voh.(none)>
2010-03-06 21:30:16 -0600

    probably dead

---
+++ htdocs/about/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+        <title>TCLUG - Twin Cities Linux Users Group</title>
+<meta http-equiv="author" content="TCLUG Webmaster">
+<meta http-equiv="Reply-to" content="webmaster@mn-linux.org">
+<meta http-equiv="Keywords" content="Twin Cities Linux Users Group, Linux, Minnesota,
+Advocacy, Linux Resources, Linux Links, Mailing List, Local Events">
+<meta http-equiv="Description" content="The Twin Cities Linux User's Group is a
+local Linux advocacy group dedicated to expanding the use of Linux, both
+professionally and personally.  The group is comprised entirely of
+volunteers.">
+<style>
+body, center, form, hr, p, td, th { font-family: Helvetica,Verdana,Sans-Serif;
+                     color: #000000;}
+
+.header {       font-family: Helvetica,Verdana,sans-serif;
+                        font-weight: bold;
+                        color: #000080;}
+
+.copy  {        font-family: Helvetica,Verdana,sans-serif;
+                        color: #999999;}
+</style>
+</head>
+
+<body bgcolor="white" background="/img/mn-linux_bg.gif" marginwidth="0" marginheight="0" topmargin="0"
+leftmargin="0">
+<table border="0" cellpadding="0" cellspacing="0" width="751">
+<tr>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt=""></td>
+</tr>
+</table>
+<table border="0" cellpadding="5" cellspacing="0" width="751">
+<tr>
+        <td width="600">
+<table width=600 border="0" align="center" cellspacing="0" cellpadding="1">
+<tr>
+  <td bgcolor="#000000" align="center">
+<table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
+<tr>
+      <td align="left"><div class="header" align="center">
+<font size=+2>About the TCLUG</font></div>
+
+The TCLUG was started in 1998 to bring Linux users in the Minneapolis area together. Over the past few years the LUG has grown from just a few members on a mailing list to a few thousand. One of the main purposes of the TCLUG is to provide a means for new users to get started with Linux. It also serves as a means for all users to communicate and get to know other Linux users in the area.
+<P>
+<B>Sponsors:</B>
+<BR>
+The TCLUG would like to thank the following companies for sponsoring the TCLUG:
+<BR>
+<A HREF="http://www.real-time.com"><IMG SRC="/images/RTELogo.gif">
+Real-Time Enterprises</A> has hosted out web site and mailing list from the early beginnings of the TCLUG.
+<P>
+<A HREF="http://www.benchmarklearning.com">Benchmark Learning</A> has provided us with meeting space since 2000.
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+<?php include("../footer.inc") ?>
+</td>
+        <td width="151" align="right" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="140">
+<tr>
+        <td align="right"><br>
+<?php include("../links.inc") ?>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<br>
+</body>
+</html>
