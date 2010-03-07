713e2fc1e13c113a45b9f6033cf9e722ffc3b4ca
John Trammell <jotr@voh.(none)>
2010-03-06 20:58:52 -0600

    from 2001_0426

---
+++ htdocs/meetings/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+        <title>TCLUG - Twin Cities Linux Users Group</title>
+<meta http-equiv="author" content="TCLUG Webmaster">
+<meta http-equiv="Reply-to" content="webmaster@mn-linux.org">
+<meta http-equiv="Keywords" content="Twin Cities Linux Users Group, Linux, Minnesota,
+Advocacy, Linux Resources, Linux Links, Mailing List, Local Events">
+<meta http-equiv="Description" content="The Twin Cities Linux User's Group is a 
+local Linux advocacy group dedicated to expanding the use of Linux, both
+professionally and personally.  The group is comprised entirely of 
+volunteers.">
+<style>
+body, center, form, hr, p, td, th { font-family: Helvetica,Verdana,Sans-Serif;  
+                     color: #000000;}
+
+.header {       font-family: Helvetica,Verdana,sans-serif;      
+                        font-weight: bold; 
+                        color: #000080;}
+
+.copy  {        font-family: Helvetica,Verdana,sans-serif;  
+                        color: #999999;}
+</style>
+</head>
+
+<body bgcolor=white background="/img/mn-linux_bg.gif" marginwidth="0" marginheight="0" topmargin="0" 
+leftmargin="0">
+<table border="0" cellpadding="0" cellspacing="0" width="751">
+<tr>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt=""></td>
+</tr>
+</table>
+<table border="0" cellpadding="5" cellspacing="0" width="95%">
+<tr>
+        <td width="95%">
+<table width=95% border="0" align="center" cellspacing="0" cellpadding="1">
+<tr>
+  <td bgcolor="#000000" align="center">
+<table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
+<tr>
+<td align="left">
+<div class="header" align="center">
+
+<font size=+2>Meeting Information</font></div>
+<BR>
+Our meetings are scheduled for the first Saturday of each month from noon until 2pm. We have been meeting at Benchmark Computing in Edina, but this is decided on a month-to-month basis.
+<P>
+Starting this month we are adding a Mid-Month Meeting which will be held
+at the University of Minnesota.  We are hoping to hold them each month
+on a Wednesday evening either the 2nd or 3rd week of the month.
+<p>
+We also have weekly beer meetings at various locations in the Twin Cities area. Check the <A HREF="/beermeeting/">Beer Meeting page</A> for more info.
+<P>
+<div class="header" align="center">
+<font size=+1>Next TCLUG Meeting</font></div>
+<P>
+<? include("nextmeeting.inc") ?>
+<P>
+<div class="header" align="center">
+<font size=+1>Past Meeting Notes</font></div>
+<P>
+<? include("pastmeetings.inc") ?>
+
+
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+<? include("../footer.inc"); ?>
+</td>
+        <td width="151" align="right" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="140">
+<tr>
+        <td align="right"><br>
+<? include("../links.inc") ?>
+</td>
+</tr>
+</table>        
+</td>
+</tr>
+</table>
+<br>
+</body>
+</html>
