5c44af70f358f7ae0f80cb307dfde771523e2719
John Trammell <jotr@voh.(none)>
2010-03-06 21:13:52 -0600

    fixed PHP tags

---
+++ htdocs/othergroups/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+        <title>TCLUG - Twin Cities Linux Users Group</title>
+<meta http-equiv="author" content="TCLUG Webmaster">
+<meta http-equiv="Reply-to" content="webmaster@mn-linux.org">
+<meta http-equiv="Keywords" content="Twin Cities Linux Users Group, Linux, Minnesota,
+Advocacy, Linux Resources, Linux Links, Mailing List, Local Events">
+<meta http-equiv="Description" content="The Twin Cities Linux User's Group is a
+local Linux advocacy group dedicated to expanding the use of Linux, both
+professionally and personally.  The group is comprised entirely of
+volunteers.">
+<style>
+body, center, form, hr, p, td, th { font-family: Helvetica,Verdana,Sans-Serif;
+                     color: #000000;}
+
+.header {       font-family: Helvetica,Verdana,sans-serif;
+                        font-weight: bold;
+                        color: #000080;}
+
+.copy  {        font-family: Helvetica,Verdana,sans-serif;
+                        color: #999999;}
+</style>
+<link REL="SHORTCUT ICON" HREF="/images/tclug.ico" >
+</head>
+
+<body bgcolor=white background="/img/mn-linux_bg.gif" marginwidth="0" marginheight="0" topmargin="0"
+leftmargin="0">
+<table border="0" cellpadding="0" cellspacing="0" width="751">
+<tr>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt="Twin Cities Linux Users Group - Minneapolis, Minnesota"></td>
+</tr>
+</table>
+<table border="0" cellpadding="5" cellspacing="0" width="95%">
+<tr>
+        <td width="95%">
+<table width=95% border="0" align="center" cellspacing="0" cellpadding="1">
+<tr>
+  <td bgcolor="#000000" align="center">
+<table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
+<tr>
+      <td align="left"><div class="header" align="center">
+<font size=+2>Other Minnesota LUGS</font></div>
+<H4>St. Cloud Area Linux Users Group</H4>
+<UL>
+<LI>Web site: <A HREF="http://scalug.mn-linux.org/">http://scalug.mn-linux.org/</A>
+</UL>
+<P>
+<H4>The Northfield Minnesota Linux Users Group (NORLUG)</H4>
+<UL>
+<LI>Web site: <A HREF="http://norlug.org/">http://norlug.org/</A>
+</UL>
+<P>
+
+<H4>The Linux Users Group of Rochester (K-LUG)</H4>
+<UL>
+<LI>Web site: <A HREF="http://www.k-lug.org">http://www.k-lug.org</A>
+</UL>
+<P>
+
+<H4>Mankato Area Unix Group (MAUG)</H4>
+<UL>
+<LI>Web site: <A HREF="http://maug.wireless.mnsu.edu/">http://maug.wireless.mnsu.edu/</A>
+</UL>
+
+<H4>Duluth Superior Linux UNIX Users Group (DSLUUG)</H4>
+<UL>
+<LI>Web site: <A HREF="http://www.dsluug.org/">http://www.dsluug.org/</A>
+</UL>
+<HR>
+
+
+<font size=+2>LUGs around the world</font></div>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.linux.org/users/index.html/">Linux Online - LUGS around the world</A></H4>
+</UL>
+
+<UL>
+<LI>                                                                                                        <H4><A HREF="http://noframes.linuxjournal.com:8080/glue/">GLUE: Groups of Linux Users Everywhere</A></H4>
+</UL>
+
+<UL>
+<LI>                                                                                                        <H4><A HREF="http://lugww.counter.li.org/lugww.php3">Linux Users Groups WorldWide</A></H4>
+</UL>
+
+
+<HR>
+<font size=+2>Other Minnesota Groups</font></div>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.tcsa.org/">Twin Cities System Administrators</A></H4>
+</UL>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.webgrrls.com/twincities_mn/">Webgrrls</A></H4>
+</UL>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.tcphp.org">Twin Cities PHP Users Group</A></H4>
+</UL>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.tcpc.com/">Twin Cities PC Users Group</A></H4>
+</UL>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.intertech-inc.com/userGroup/javaUserGroup.asp">
+Twin Cities Java Users Group</A></H4>
+</UL>
+
+<UL>
+<LI>
+<H4><A HREF="http://www.uum.org/">UNIX Users of Minnesota</A></H4>
+</UL>
+
+
+
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+<?php include("../footer.inc") ?>
+</td>
+        <td width="151" align="right" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="140">
+<tr>
+        <td align="right"><br>
+<?php include("../links.inc") ?>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<br>
+</body>
+</html>
