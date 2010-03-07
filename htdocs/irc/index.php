236045d1ce40bfff2e579c0fdfda5537b8e6656a
John Trammell <jotr@voh.(none)>
2010-03-06 21:21:28 -0600

    fixed php tags

---
+++ htdocs/irc/index.php
@@ -0,0 +1,53 @@
+<?php
+$title="TCLUG - Twin Cities Linux Users Group";
+include "../header.inc";
+?>
+<table border="0" cellpadding="0" cellspacing="0" width="751">
+<tr>
+        <td><img src="/images/tclugminn.jpg" width=500 height=150 border=0 alt="Twin Cities Linux Users Group - Minneapolis, Minnesota"></td>
+</tr>
+</table>
+<table border="0" cellpadding="5" cellspacing="0" width="95%">
+<tr valign=top>
+        <td width="95%">
+<table width=95% border="0" align="center" cellspacing="0" cellpadding="1">
+<tr>
+  <td bgcolor="#000000" align="center">
+<table width=100% cellspacing=0 cellpadding=20 border=0 bgcolor="ivory">
+<tr>
+      <td align="left"><div class="header" align="center">
+<font size=+2>TCLUG IRC Channel</font></div>
+<P>
+Have a Linux question you want answered right away? Or do you just want to chat with other TCLUG members? Then the IRC channel is the place for you.
+<P>
+Server: <B>irc.freenode.net</B>
+<BR>
+Channel: <B>#tclug</B>
+<P>
+For more information on IRC, check out the following links:<BR>
+<A HREF="http://www.irchelp.org/">http://www.irchelp.org/</A><BR>
+<A HREF="http://www.mirc.co.uk/irc.html">http://www.mirc.co.uk/irc.html</A><BR>
+</td>
+</tr>
+</table></td>
+</tr>
+</table>
+
+<?php include("../footer.inc") ?>
+
+
+</td>
+        <td width="151" align="right" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="140">
+<tr>
+        <td align="right"><br>
+<?php include("../links.inc") ?>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<br>
+</body>
+</html>
